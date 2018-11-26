94a75ebf845624316e0afbe869881f4b50bd9386
jdreyespaez <jd.reyespaez@gmail.com>
2018-11-26 11:46:48 -0500

    Estructura básica de la conexión a DB con middleware

--- conexion/conexion.js
+++ conexion/conexion.js
@@ -3,3 +3,30 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 // Agregamos el manejador de rutas
+const routes = require('../rutas/personasRutas')
+
+const app = express()
+const config = require('./db')
+const PORT = 4000;
+
+// CONEXIÓN A LA BD 
+mongoose.connect(config.DB, {useNewUrlParser: true}). then(
+    () => {console.log(`Conexión Exitosa :'D`)},
+    // Este salto de línea se ve en la consola
+    error => {console.log(`Error en la conexión:
+                            ${error}`)
+});
+
+app.use(bodyParser.json())
+app.use('/api', routes)
+
+// MIDDLEWARE
+app.use(function(err, req, res, next){
+    res.status(422),send({error: err.message})
+})
+
+
+// Conexión a puerto
+app.listen(PORT, function(){
+    console.log(`Corriendo en el Puerto: ${PORT}`);
+})
