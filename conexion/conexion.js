f8780dc98f2b355473180743b0b75beac117cee8
jdreyespaez <jd.reyespaez@gmail.com>
2018-11-26 11:40:13 -0500

    Estructura básica del API y archivo de conexión de Node con Mongo

---
+++ conexion/conexion.js
@@ -0,0 +1,5 @@
+// Llamamos a todas la librerías
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+// Agregamos el manejador de rutas
---
+++ conexion/db.js
@@ -0,0 +1,4 @@
+// Este es el puerto propio de la BD
+module.exports = {
+    DB: 'mongodb://localhost:27017/personas'
+}
